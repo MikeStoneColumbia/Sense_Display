e450fff8038b2451a0713d6a55eec35ca0729ed6
Michael Stone <ms5512"columba.edu>
2021-03-11 05:20:49 +0000

    ngrok

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, jsonify
-
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+run_with_ngrok(app)
 
 @app.route('/', methods=['GET'])
 def display_ui():
@@ -21,6 +22,5 @@ def disaply_senseHat():
     return jsonify(response='success') # return success if we load
 
 
-
 if __name__ == '__main__':
-    app.run(debug=True,host='127.0.0.1')
+    app.run()
